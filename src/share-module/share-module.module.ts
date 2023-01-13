ebb0d2c2069df2b338f1a4ad916ae57cfede4661
Sebastian Bolaños <sbolanosm.codyd@gmail.com>
2023-01-13 10:40:03 -0500

    feat: carpeta test con controlador y servicios

--- src/share-module/share-module.module.ts
+++ src/share-module/share-module.module.ts
@@ -1,5 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TestController } from './test/test.controller';
+import { TestService } from './test/test.service';
 
 @Module({
+
+  controllers: [TestController],
+
+  providers: [TestService]
 })
 export class ShareModuleModule {}
