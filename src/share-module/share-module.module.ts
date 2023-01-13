c883c899b665f41ae616c86f646f617af17e8a3b
Sebastian Bolaños <sbolanosm.codyd@gmail.com>
2023-01-13 10:54:36 -0500

    feat: configuramos la clase que inyectara los metodos en 'share-module' con los headers parametrizados en http

--- src/share-module/share-module.module.ts
+++ src/share-module/share-module.module.ts
@@ -1,8 +1,34 @@
+import { HttpModule } from '@nestjs/axios';
+
+//Ahora si hacemos uso de la clase que creamos, que es la clase inyectable 
+//que hará uso de los headers configurados.
+
+/**
+ * Esto quiere decir que todos los metodos http que pasen por este archivo share-module
+ * se le inyecten los headers que tenemos configurados en 'HttpConfigService'
+ */
+
+import { HttpConfigService } from 'src/httpService.config';
+
 import { Module } from '@nestjs/common';
 import { TestController } from './test/test.controller';
 import { TestService } from './test/test.service';
 
 @Module({
+
+  //configuramos un import:
+  /**
+   * Y entonces aqui hacemos uso de la clase 'HttpConfigService' que contiene los
+   * headers parametrizados
+   * E importamos la clase 'ShareModuleModule' de este mismo archivo 'share-module'
+   */
+  imports: [
+    HttpModule.registerAsync({
+      useClass : HttpConfigService,
+    }),
+    ShareModuleModule
+
+  ],
 
   controllers: [TestController],
 
